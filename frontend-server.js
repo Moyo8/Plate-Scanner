6b861d38d9cfc699fecc91d316d60226bf69e19a
Moyo8 <adeyefamoyo@gmail.com>
2025-10-07 10:00:36 +0100

    changed the root

--- frontend-server.js
+++ frontend-server.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
 const frontendPath = path.join(__dirname, 'frontend');
 
-app.use(express.static(frontendPath));
+// Do not let express.static automatically serve index.html so we can control the root
+app.use(express.static(frontendPath, { index: false }));
 app.get('/', (req, res) => res.sendFile(path.join(frontendPath, 'landing-page.html')));
 
 app.listen(PORT, () => console.log(`Frontend server running at http://localhost:${PORT}`));
